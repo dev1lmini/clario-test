930fa8361a5cb3d8c72c3da856aa043d1d86ec71
Markiian Linynskyy <devilmini.ru@gmail.com>
2024-12-19 18:36:48 +0200

    feat(configuration): setup theme, packages and project
     - add tailwind styles theme
     - setup project packages
     - update project name
     - setup default styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"]
+      },
+      fontSize: {
+        "3xl": 28
+      },
+      colors: {
+        primary: {
+          100: "#70C3FF",
+          200: "#4B65FF"
+        },
+        secondary: {
+          100: "#F4F9FF",
+          200: "#E0EDFB"
+        },
+        midnight: "#4A4E71",
+        sky: "#6F91BC",
+        emerald: "#27B274",
+        coral: "#FF8080",
+        blush: "#FDEFEE",
+        light: "#CFE1F4"
+      }
+    }
   },
   plugins: []
 }
