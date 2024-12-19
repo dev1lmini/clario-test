3120b974319c5323551a912a5000f5ac500fdc7c
Markiian Linynskyy <devilmini.ru@gmail.com>
2024-12-19 18:42:07 +0200

    feat(login): add login screen to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Login } from "./Login"
+import bgStars from "./assets/bg-stars.svg"
+
 function App() {
   return (
-    <main>
+    <main className="flex overflow-hidden px-7 flex-col justify-center items-center bg-gradient-to-br h-svh md:h-full md:min-h-screen  from-secondary-100  to-secondary-200 relative">
       <title>Clario test showcase</title>
+      <img src={bgStars} className="absolute md:top-auto top-16" />
+      <Login />
     </main>
   )
 }
